610230219334c77dbd6fcc32e96fc40134705f74
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-01-22 15:44:56 +0100

    Update StreamDeckOptions.cs

--- VBANDeck/Model/StreamDeckOptions.cs
+++ VBANDeck/Model/StreamDeckOptions.cs
@@ -1,19 +1,14 @@
 using CommandLine;
 
-namespace VBANDeck.Model
-{
-    public class StreamDeckOptions
-    {
-        [Option("port", Required = true)]
-        public int Port { get; set; }
-        
-        [Option("uuid", Required = false)]
-        public string Uuid { get; set; }
-        
+namespace VBANDeck.Model {
+    public class StreamDeckOptions {
+        [Option("port", Required = true)] public int Port { get; set; }
+
+        [Option("uuid", Required = false)] public string Uuid { get; set; }
+
         [Option("registerEvent", Required = false)]
         public string RegisterEvent { get; set; }
-        
-        [Option("info", Required = false)]
-        public string Info { get; set; }
+
+        [Option("info", Required = false)] public string Info { get; set; }
     }
 }
