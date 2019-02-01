ea7467212fa98602f623d43aff7168cacce915ee
Tobias Burdow [Kaleidox] <42473485+burdoto@users.noreply.github.com>
2019-02-01 13:49:40 +0100

    Update build_exe.bat

--- VBANDeck/build_exe.bat
+++ VBANDeck/build_exe.bat
@@ -1,14 +1,8 @@
 @echo off
-if [%1]=="-r" (
-    goto :Release
-) else (
-    goto :Debug
-)
+goto :Release
 
 :Debug
-dotnet publish -c Debug -r win10-x64
-dotnet publish -c Debug -r ubuntu.16.10-x64
+dotnet publish -c Debug -r win-x86
 
 :Release
-dotnet publish -c Release -r win10-x64
-dotnet publish -c Release -r ubuntu.16.10-x64
+dotnet publish -c Release -r win-x86
